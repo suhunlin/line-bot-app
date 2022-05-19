a7c6fdb7dc3f27a5f6e080cfa7575727a0811903
suhunlin <lin.suhun@msa.hinet.net>
2022-05-19 20:45:57 +0800

    fix bug miss .self

--- line_sticker_handler.py
+++ line_sticker_handler.py
@@ -7,27 +7,27 @@ class LineStickerHandler:
 
     def handle_sticker_from_text_message(self):
         sticker_message = None
-        if '貼圖' in msg:
+        if '貼圖' in self.msg:
             sticker_message = StickerSendMessage(
                 package_id='446',
                 sticker_id='2004'
             )
-        elif '品妤' in msg:
+        elif '品妤' in self.msg:
             sticker_message = StickerSendMessage(
                 package_id='789',
                 sticker_id='10856'
             )
-        elif '冠儒' in msg:
+        elif '冠儒' in self.msg:
             sticker_message = StickerSendMessage(
                 package_id='6632',
                 sticker_id='11825377'
             )
-        elif '麗君' in msg:
+        elif '麗君' in self.msg:
             sticker_message = StickerSendMessage(
                 package_id='6632',
                 sticker_id='11825376'
             ) 
-        elif '書弘' in msg:
+        elif '書弘' in self.msg:
             sticker_message = StickerSendMessage(
                 package_id='6632',
                 sticker_id='11825384'
